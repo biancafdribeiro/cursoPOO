1d3fa4a5a768ef3d4ede59e050a5d26936af2239
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-08-28 05:09:30 -0300

    Aula 4

---
+++ Curso_POO_Aula4/CartaoDeCredito.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_POO_Aula4
+{
+    public class CartaoDeCredito
+    {
+        public string Numero { get; set; }
+        public string DataDeValidade { get; set; }
+        public Cliente Cliente { get; set; } //chamando a classe Cliente (agregação). a classe atual possui
+                                             //uma propriedade que referencia um objeto da classe Cliente. 
+    }
+}
---
+++ Curso_POO_Aula4/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_POO_Aula4
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+    }
+}
